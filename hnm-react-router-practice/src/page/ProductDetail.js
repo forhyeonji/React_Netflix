b4bf3821f71d71b85aa8811bf04621d214b02998
forhyeonji <ghkdguswl3@gmail.com>
2022-10-17 23:52:39 +0900

    redux middleware

--- hnm-react-router-practice/src/page/ProductDetail.js
+++ hnm-react-router-practice/src/page/ProductDetail.js
@@ -1,33 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useDispatch, useSelector } from "react-redux";
+import { productAction } from "../redux/actions/productAction";
+
 
 const ProductDetail = () => {
+
   let { id } = useParams();
-  const [product, setProduct] = useState(null);
-  const getProductDetail = async () => {
-    let url = `http://localhost:5001/products/${id}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    setProduct(data);
+  const detailList = useSelector(state => state.product.detailList);
+  const dispatch = useDispatch();
+ 
+  
+  const getProductDetail = () => {
+    dispatch(productAction.getProductDetail({id}));
   };
+
 
   useEffect(() => {
     getProductDetail();
   }, []);
 
+
   return (
     <Container>
       <Row>
         <Col className="product-img">
-          <img src={product?.img} />
+          <img src={detailList?.img} />
         </Col>
         <Col>
-          <div>{product?.title}</div>
-          <div>{product?.price}</div>
-          <div>{product?.choice ? "Conscious Choice" : ""}</div>
+          <div>{detailList?.title}</div>
+          <div>{detailList?.price}</div>
+          <div>{detailList?.choice ? "Conscious Choice" : ""}</div>
           <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
               사이즈 선택
