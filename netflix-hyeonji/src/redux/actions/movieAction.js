397c1a8c0811a046a3156e27310612696d03ba12
forhyeonji <ghkdguswl3@gmail.com>
2022-10-18 16:43:54 +0900

    넷플릭스 뼈대

---
+++ netflix-hyeonji/src/redux/actions/movieAction.js
@@ -0,0 +1,12 @@
+function getMovies(){
+    return async(dispatch)=>{
+        let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`;
+        let response = await fetch(url);
+        let data = await response.json();
+
+        let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
+        
+
+        let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
+    }
+}
