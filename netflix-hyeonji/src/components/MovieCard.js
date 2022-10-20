b527e64c7d40ea55187624c7584d78adaf584c3f
forhyeonji <ghkdguswl3@gmail.com>
2022-10-20 23:40:13 +0900

    card detail

--- netflix-hyeonji/src/components/MovieCard.js
+++ netflix-hyeonji/src/components/MovieCard.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Badge } from "react-bootstrap";
 
-const MovieCard = ({item}) => {
+const MovieCard = ({ item }) => {
   return (
     <div
-        className='card'
-        style={{
-            backgroundImage:
-            "url(" + `https://www.themoviedb.org/t/p/w710_and_h400_multi_faces${item.backdrop_path}` + ")",
-        }}
-        >
-    </div>
-  )
-}
+      className="card"
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://www.themoviedb.org/t/p/w710_and_h400_multi_faces${item.backdrop_path}` +
+          ")",
+      }}
+    >
+      <div className="overlay">
+            <h2>{item.title}</h2>
+            <div>
+                {item.genre_ids.map((id) => (
+                    <Badge bg="danger">{id}</Badge>
+                ))}
+            </div>
+            <div>
+                <span>{item.vote_average}</span>
+                <span>{item.adult?"청불":"Under 18"}</span>
+            </div>
 
-export default MovieCard
+      </div>
+    </div> /* 백그라운드 */
+  );
+};
+
+export default MovieCard;
