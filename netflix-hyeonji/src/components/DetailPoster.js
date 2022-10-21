c08e38de01910825ec81c918d049b0ab5e16615f
forhyeonji <ghkdguswl3@gmail.com>
2022-10-21 16:47:24 +0900

    get movie poster through 'useParams','find()'

--- netflix-hyeonji/src/components/DetailPoster.js
+++ netflix-hyeonji/src/components/DetailPoster.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 //1. 디테일 페이지에서 정보를 받아온다
 //2. 디테일포스터 - 디테일페이지 순으로 작업해보자
@@ -7,10 +8,14 @@ import React from 'react'
 //searchParams를 통해서 id값을 매치하자!!!
 //일단 동노가서 스트레스 좀 풀고..!!
 
+//마치기 전에 혹시나 해봤는데 된다..ㅠ1!!!성공!!!!
+
 
 const DetailPoster = ({movies}) => {
     console.log("포스터!!!",movies);
-
+    const params = useParams();
+    const imgUrl = movies.data.results.find(item => item.id==params.id).backdrop_path;
+    console.log("과연 찾았니??", imgUrl)
 
     return (
     <div>
@@ -19,11 +24,11 @@ const DetailPoster = ({movies}) => {
             style={{
             backgroundImage:
             "url("+
-            `https://www.themoviedb.org/t/p/w710_and_h400_multi_faces${movies.data.results[0].backdrop_path}`+")"}}
+            `https://www.themoviedb.org/t/p/w710_and_h400_multi_faces${imgUrl}`+")"}}
             >
             
             
-            <p>{movies.data.results[0].id}</p>
+            <p>{params.id}</p>
         </div>
     </div>
   )
