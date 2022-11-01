d4e772fbe5b16ca74ad448b943ec1c95ae3240b7
forhyeonji <ghkdguswl3@gmail.com>
2022-11-01 16:47:57 +0900

    search

--- netflix-hyeonji/src/redux/actions/movieAction.js
+++ netflix-hyeonji/src/redux/actions/movieAction.js
@@ -1,7 +1,11 @@
+import { faBlackboard } from "@fortawesome/free-solid-svg-icons";
 import api from "../api";
 import { movieActions } from "../reducers/movieReducer";
 
 
+// 검색기능 따로 api 받아와야함
+// search로 하는 &query=black
+// 이런식으로 url 맨 마지막에 붙는다
 
 const API_KEY=process.env.REACT_APP_API_KEY
 
@@ -29,6 +33,7 @@ function getMovies({ currentpage }){
         const genreApi = api.get(
             `/genre/movie/list?api_key=${API_KEY}&language=en-US`
             );
+
 
     
 
