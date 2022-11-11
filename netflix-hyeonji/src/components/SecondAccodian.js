c6dc56f31ea2ad845b184c702cb79b624080451f
forhyeonji <ghkdguswl3@gmail.com>
2022-11-11 16:49:51 +0900

    input range 구현중

--- netflix-hyeonji/src/components/SecondAccodian.js
+++ netflix-hyeonji/src/components/SecondAccodian.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
+import InputRange from 'react-input-range';
 
 export const SecondAccodian = () => {
   return (
@@ -10,7 +11,16 @@ export const SecondAccodian = () => {
             <Accordion.Header>
               <h5>Filter</h5>
             </Accordion.Header>
-            <Accordion.Body className="accordian">안녕</Accordion.Body>
+            <Accordion.Body className="accordian">
+                {/* <InputRange
+                    maxValue={20}
+                    minValue={0}
+                    value={this.state.value}
+                    onChange={value => this.setState({ value })}
+                /> */}
+        
+        
+            </Accordion.Body>
             <Accordion.Body className="accordian">메롱</Accordion.Body>
             <Accordion.Body className="accordian">하이</Accordion.Body>
           </Accordion.Item>
