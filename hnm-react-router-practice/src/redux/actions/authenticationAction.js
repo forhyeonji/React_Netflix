cafed7df58bccc0eb6e8bf98656230cb9c7c330e
forhyeonji <ghkdguswl3@gmail.com>
2022-11-25 01:16:53 +0900

    for posting site

--- hnm-react-router-practice/src/redux/actions/authenticationAction.js
+++
@@ -1,9 +0,0 @@
-function login(id, password){
-
-    return (dispatch, getState)=>{
-        console.log("login success action");
-        dispatch({type : "LOGIN-SUCCESS", payload:{id, password }});
-    };
-}
-
-export const authenticationAction = { login };
